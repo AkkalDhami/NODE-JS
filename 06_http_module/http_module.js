5d2a0c9b766cdbdf794a83e97892b515b84e31df
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-20 20:09:10 +0545

    06_http_module

--- 06_http_module/http_module.js
+++ 06_http_module/http_module.js
@@ -2,12 +2,13 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     if (req.url === "/") {
+
         res.setHeader("Content-Type", "text/html");
         res.write("<h1>Welcome to our home page</h1> <h2>This is our home page</h2>");
         res.end();
     }
     if (req.url === "/source") {
-        res.write("Welcome to our source page.  This is our source page");
+        res.write("Welcome to our source page.  This is our source page.");
         res.end();
     }
     if (req.url === "/contact") {
