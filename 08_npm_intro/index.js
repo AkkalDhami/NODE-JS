8547c5616a33df526425d83f7178f30e508d1e44
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-23 20:43:06 +0545

    npm semantic versions

---
+++ 08_npm_intro/index.js
@@ -0,0 +1,21 @@
+//? Semantic Versioning System
+
+//? Major, Minor, Patch
+
+//? 1.0.0
+
+//? last 0: patch -> small changes
+
+//? Second last 0: minor -> minor changes
+
+//? 1: major -> major changes, break changes
+
+//? caret(^): minor and patch up to next minor allowed
+
+//? tilde(~): ONLY patch are allowed
+
+//? exclamation(!): major
+
+//? exact: fixed version
+
+//? x: wildcard for minor and patch
