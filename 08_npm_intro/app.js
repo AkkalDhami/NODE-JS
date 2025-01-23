3dea8317d1467d3788371e04060634e0851852ba
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-23 20:42:51 +0545

    npm intro

---
+++ 08_npm_intro/app.js
@@ -0,0 +1,21 @@
+//? npm is a popular package manager which comes bundled with Node.js
+
+//?  It is a CLI tool used to install, update, and remove external packages
+
+//?  Do not confuse npm CLI with npmjs.com , as it’s a registry where most of the  packages of Node.js are saved.
+
+//?  node_modules is what stores all the installed packages
+
+//! npm commands:
+//* npm install or npm i
+
+//* npm list : to list all the dependencies packages
+
+//* npm list -a: to list all the dependencies packages including dev dependencies
+
+//* npm list -g: to list all the globally installed packages
+
+//* npm view packageName(react, express) : to view the details of a package
+
+//* npm view packageName(chalk) versions: to view the versions of a package
+
