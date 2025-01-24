559e9231406c142bd12ef9653985bf3ce03be4ca
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-24 15:02:18 +0545

    npm commands

--- 08_npm_intro/app.js
+++ 08_npm_intro/app.js
@@ -19,3 +19,36 @@
 
 //* npm view packageName(chalk) versions: to view the versions of a package
 
+//* npm view packageName@version: to view the details of a specific version of a package
+
+//? npm install express@4.0.0 // ^4.0.0
+
+//?  • npm install express@4.0.0 --save-exact // To install exact version. (^ removed)
+
+//?  • npm install express@~4.0.0
+
+//?  • npm install express@4.2.x
+
+//?  • You can use any symbols while specifying version
+
+//? npm outdated: shows outdated packages in your project.
+
+//? npm remove <package-name>
+
+//? npm update
+
+//? npm update
+
+//? npx npm-check-updates:  upgrade your packages to absolute latest versions
+
+//? npx npm-check-updates -u // To update the packages after reviewing.
+
+//? npm install -g: to install a package globally
+
+//? npm install -g npm-check-updates: to install npm-check-updates globally
+
+//?  npm-check-updates
+
+//?  npm outdated -g: to check outdated packages globally
+
+//? npm install -D eslint: install eslint as a development dependency.
