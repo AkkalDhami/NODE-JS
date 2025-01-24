a212aaa7db0910a9e055c14a2f7497232291a115
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-24 15:02:23 +0545

    npm commands

---
+++ 09_npm_commands/app.js
@@ -0,0 +1,17 @@
+//? npm install express@4.0.0 // ^4.0.0
+
+//?  • npm install express@4.0.0 --save-exact // To install exact version. (^ removed)
+
+//?  • npm install express@~4.0.0
+
+//?  • npm install express@4.2.x
+
+//?  • You can use any symbols while specifying version
+
+//? npm outdated
+//  • It shows outdated packages in your project.
+//  • Current: The version of the package currently installed in your project.
+//  • Wanted: The latest version that satisfies the version range defined in your package.json. 
+// It basically uses the symbols to get it.
+//  THAPA TECHNICAL – NODEJS YOUTBE SERIES
+//  • Latest: Absolute latest version of the package
