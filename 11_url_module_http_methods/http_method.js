581deb2d4a92c08445eb279b55c5366dfa4fe5aa
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-26 20:54:56 +0545

    http methods

---
+++ 11_url_module_http_methods/http_method.js
@@ -0,0 +1,7 @@
+//? HTTP Methods: GET, POST, PUT, PATCH, DELETE
+
+//? GET: Retrieve data from a server, by default GET method is used
+//? POST: Send data to a server
+//? PUT: Update data on a server, file photo upload
+//? PATCH: Partially update data on a server, change existing entry
+//? DELETE: Remove data from a server
