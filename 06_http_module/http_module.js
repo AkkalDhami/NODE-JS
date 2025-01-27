9de144ec0bf3309b7897e104ee2f220d1d3d6ced
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-27 19:14:51 +0545

    update http_module.js

--- 06_http_module/http_module.js
+++ 06_http_module/http_module.js
@@ -1,26 +1,27 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-
-        res.setHeader("Content-Type", "text/html");
-        res.write("<h1>Welcome to our home page</h1> <h2>This is our home page</h2> <p> This is our home page</p>");
-        res.end();
-    }
-    if (req.url === "/source") {
-        res.write("Welcome to our source page.  This is our source page.");
-        res.end();
-    }
-    if (req.url === "/contact") {
-        res.setHeader("Content-Type", "text/html");
-        res.write("Welcome to our contact page.  This is our contact page");
-        res.end();
-    }
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      "<h1>Welcome to our home page</h1> <h2>This is our home page</h2> <p> This is our home page</p>"
+    );
+    res.end();
+  }
+  if (req.url === "/source") {
+    res.write("Welcome to our source page.  This is our source page.");
+    res.end();
+  }
+  if (req.url === "/contact") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("Welcome to our contact page.  This is our contact page");
+    res.end();
+  }
 });
 
 //web server
 
 const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
