ebe0957d90e6fe12f4c006e085dbb43e2326ad16
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-27 19:15:30 +0545

    make currency convertor

---
+++ 12_CLI_projects_II/currencyConvertor.js
@@ -0,0 +1,8 @@
+import https from 'https';
+import chalk from 'chalk';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
