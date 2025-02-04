eed32a821c3b8c906fe717f0ca03227bafdb3e10
AkkalDhami <dhamiakkal21@gmail.com>
2025-02-04 21:04:07 +0545

    introduction to express.js

---
+++ 17_intro_to_express.js/app.js
@@ -0,0 +1,26 @@
+//? Express.JS: s a minimal and flexible web application framework for Node.js.
+
+//?  It provides a robust set of featuresmfor building single - page,    multi page, and hybrid webapplications.
+
+//? Express.js simplifies server - side coding by providing a layer of fundamental web applicationfeatures.
+
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.send("<h1>Welcome to our home page </h1>");
+});
+
+app.get("/about", (req, res) => {
+    return res.send("<h1>Welcome to our about page </h1>");
+});
+
+app.get("/contact", (req, res) => {
+    return res.send("<h1>Welcome to our contact page </h1>");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
