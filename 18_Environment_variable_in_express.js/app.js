ebc380a4d5f86f2a8540c3dc6e810e47c8e40bdc
AkkalDhami <dhamiakkal21@gmail.com>
2025-02-06 19:31:19 +0545

    environment variables

---
+++ 18_Environment_variable_in_express.js/app.js
@@ -0,0 +1,18 @@
+//? Environment variables are used to store configuration values like port number, API keys, database URLs, or secrets outside the codebase.
+
+//? Most platforms automatically add PORT environment variable which we can use.
+
+// console.log(process.env);
+
+import express from 'express'
+
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.send("<h1>Welcome to our home page </h1>");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
