c10f18e7ae6e32467c85bc27ac4f0dbe2f6ec193
AkkalDhami <dhamiakkal21@gmail.com>
2025-02-07 20:49:17 +0545

    add zod validation to port

---
+++ 18_Environment_variable_in_express.js/port.js
@@ -0,0 +1,5 @@
+// export const PORT = isNaN(process.env.PORT) ? 3000 : parseInt(process.env.PORT);
+import { z } from "zod";
+
+const VALID_PORT = z.number().min(1).max(6000).default(3000);
+export const PORT = VALID_PORT.parse(process.env.PORT);
