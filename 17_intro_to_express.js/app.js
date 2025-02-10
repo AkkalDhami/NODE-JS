683e597c47963c6039eee389ab9bf6138262a5f9
AkkalDhami <dhamiakkal21@gmail.com>
2025-02-10 18:39:29 +0545

    update code

--- 17_intro_to_express.js/app.js
+++ 17_intro_to_express.js/app.js
@@ -5,24 +5,15 @@
 //? Express.js simplifies server - side coding by providing a layer of fundamental web applicationfeatures.
 
 import express from 'express';
-
 const app = express();
-
 app.get("/", (req, res) => {
-
     return res.send("<h1>Welcome to our home page </h1>");
 });
-
 app.get("/about", (req, res) => {
-
     return res.send("<h1>Welcome to our about page </h1>");
 });
-
-app.get("/contact", (req, res) => {
-    return res.send("<h1>Welcome to our contact page </h1>");
-});
-
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
