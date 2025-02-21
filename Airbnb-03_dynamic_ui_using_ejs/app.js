26196d383c62278731a300cdd93f36bc24be0557
AkkalDhami <dhamiakkal21@gmail.com>
2025-02-21 18:38:30 +0545

    dynamic ui in ejs

--- Airbnb-03_dynamic_ui_using_ejs/app.js
+++ Airbnb-03_dynamic_ui_using_ejs/app.js
@@ -23,7 +23,7 @@ app.set("views", "views");
 
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "404.ejs"));
+  res.status(404).render("404", { title: "Page Not Found" });
 })
 
 const PORT = 3000;
