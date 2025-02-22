d57e67dde9d506afb4521dac9f7ba38c2e984e9d
AkkalDhami <dhamiakkal21@gmail.com>
2025-02-22 12:34:13 +0545

    practise set dynamic ui in ejs

---
+++ Practise Set/Airbnb-03_dynamic_ui_using_ejs/routes/userRouter.js
@@ -0,0 +1,12 @@
+
+import express from 'express'
+const userRouter = express.Router();
+
+import { registeredHomes } from '../routes/hostRouter.js';
+
+userRouter.get("/", (req, res, next) => {
+  res.render('home', { registeredHomes: registeredHomes, title: 'Airbnb Home' ,currentPage: 'Home'});
+  console.log({ registeredHomes })
+});
+
+export { userRouter };
