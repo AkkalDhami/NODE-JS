7423f5a71a32842032d8c000146b1119b28b5765
AkkalDhami <dhamiakkal21@gmail.com>
2025-06-02 10:38:41 +0545

    Create app.js

---
+++ 29_ejs_in_url_shortener/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { shortnerRoutes } from "./routes/shortner.routes.js";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views');
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use("/", shortnerRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
