7f3268bc7f8ca6c54cd4ce7b1413346a8fd2e0cc
AkkalDhami <dhamiakkal21@gmail.com>
2025-06-13 06:31:26 +0545

    Create storeController.js

---
+++ Airbnb-06-Dynamic Paths-I/controllers/storeController.js
@@ -0,0 +1,77 @@
+import Home from '../models/home.js';
+import Favourite from '../models/favourite.js';
+export const getHomes = (req, res, next) => {
+    Home.fetchAll(registeredHomes =>
+        res.render('store/home-list', {
+            registeredHomes: registeredHomes,
+            title: 'Airbnb Home',
+            currentPage: 'home'
+        })
+    );
+}
+export const getIndex = (req, res, next) => {
+    Home.fetchAll(registeredHomes =>
+        res.render('store/index', {
+            registeredHomes: registeredHomes,
+            title: 'Homes List',
+            currentPage: 'index'
+        })
+    );
+}
+export const getBookings = (req, res, next) => {
+    res.render('store/bookings', {
+        title: 'My Bookings',
+        currentPage: 'bookings'
+    })
+}
+
+export const getFavouriteList = (req, res, next) => {
+    Favourite.getFavourites(favourites => {
+        Home.fetchAll(registeredHomes => {
+            const favouriteHomes = registeredHomes.filter(home => favourites.includes(home.id)) || [];
+            console.log("Favourite Homes: ", favouriteHomes);
+            res.render('store/favourite-list', {
+                favouriteHomes: favouriteHomes,
+                title: 'My Favourites',
+                currentPage: 'favourites'
+            });
+
+        });
+
+    });
+
+}
+
+export const getHomeDetail = (req, res, next) => {
+    const homeId = req.params.homeId;
+
+    Home.findHomeById(homeId, homeFound => {
+        console.log("Home Found: ", homeFound);
+        if (!homeFound) {
+            console.log("Home not found");
+            res.redirect('/homes');
+
+        } else {
+            res.render('store/home-detail', {
+                home: homeFound,
+                title: 'Home Detail',
+                currentPage: 'home'
+            });
+        }
+
+    });
+
+}
+
+export const postAddToFavouriteList = (req, res, next) => {
+    const homeId = req.body.homeId;
+    console.log(homeId);
+    Favourite.addToFavourites(homeId, added => {
+        if (added) {
+            res.redirect('/favourite-list');
+        } else {
+            res.redirect('/homes');
+        }
+    });
+
+}
