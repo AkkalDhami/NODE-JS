cac5890729c0618219931c731fb9e39776cdbd8a
AkkalDhami <dhamiakkal21@gmail.com>
2025-06-22 08:50:23 +0545

    Create app.js

---
+++ Notes-App-MySQL/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { noteRouter } from "./routes/noteRouter.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+
+app.use(noteRouter);
+
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
