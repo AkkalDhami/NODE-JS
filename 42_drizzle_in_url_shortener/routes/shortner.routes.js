b2e4feba7a629a2ce330f948bee9255a98bffa40
AkkalDhami <dhamiakkal21@gmail.com>
2025-06-26 07:35:38 +0545

    Create shortner.routes.js

---
+++ 42_drizzle_in_url_shortener/routes/shortner.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { postURLShortener, getShortenerPage, reDirectToShortLinks, deleteShortLink } from "../controllers/postURLShortens.controller.js";
+const router = Router();
+
+
+router.post('/', postURLShortener);
+router.get('/', getShortenerPage);
+router.get("/:shortCode", reDirectToShortLinks);
+router.get("/delete/:shortCode", deleteShortLink);
+
+export const shortnerRoutes = router;
