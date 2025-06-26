b4fcdecf1707f63ac5f5f1840f74d7d62e2cf2b6
AkkalDhami <dhamiakkal21@gmail.com>
2025-06-26 07:35:07 +0545

    Create postURLShortens.controller.js

---
+++ 42_drizzle_in_url_shortener/controllers/postURLShortens.controller.js
@@ -0,0 +1,63 @@
+import crypto from "crypto";
+
+import { loadLinks, saveLinks, getLinkByShortCode, deleteLink } from "../models/shortener.model.js";
+
+
+export const getShortenerPage = async (req, res) => {
+    try {
+        const links = await loadLinks()
+        return res.render("index", { links, host: req.hostname });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Internal server error");
+    }
+};
+
+export const postURLShortener = async (req, res) => {
+    try {
+        const { url, shortCode } = req.body;
+        const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+
+        const links = await loadLinks()
+
+        if (links[finalShortCode]) {
+            return res
+                .status(400)
+                .send("Short code already exists. Please choose another.");
+        }
+
+        await saveLinks({ url, shortCode: finalShortCode });
+
+        return res.redirect("/");
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send(error.message);
+    }
+};
+
+export const reDirectToShortLinks = async (req, res) => {
+    try {
+        const { shortCode } = req.params;
+        console.log("shortCode2", shortCode);
+        const link = await getLinkByShortCode(shortCode);
+        
+        console.log("link", link);
+
+        if (!link) return res.status(404).send("404 error occurred");
+
+        return res.redirect(link.url);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send("Internal server error");
+    }
+};
+export const deleteShortLink = async (req, res) => {
+    try {
+        const { shortCode } = req.params;
+        await deleteLink(shortCode);
+        return res.redirect("/");
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send("Internal server error");
+    }
+};
