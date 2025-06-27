55ba4ed4fbd53d11c7729b39bc700d68cc4791e8
AkkalDhami <dhamiakkal21@gmail.com>
2025-06-27 07:31:48 +0545

    Add shortener model for URL shortener app
    Introduces a model for managing short links, including functions to load, save, retrieve by short code, and delete links using the database and Drizzle ORM.

---
+++ 44_Cokkies_Sessions_in_url_shortener/models/shortener.model.js
@@ -0,0 +1,26 @@
+import db from "../db.js";
+import { shortLinksTable } from "../drizzle/schema.js";
+import { eq } from 'drizzle-orm';
+export const loadLinks = async () => {
+    return await db.select().from(shortLinksTable);
+};
+
+export const saveLinks = async (links) => {
+    console.log("links", links);
+    return await db.insert(shortLinksTable).values({
+        shortCode: links.shortCode,
+        url: links.url
+    });
+};
+
+export const getLinkByShortCode = async (shortCode) => {
+    console.log("shortCode", shortCode);
+    const [link] = await db.select().from(shortLinksTable).where({
+        shortCode: shortCode,
+    });
+    return link;
+};
+
+export const deleteLink = async (shortCode) => {
+    await db.delete(shortLinksTable).where(eq(shortLinksTable.shortCode, shortCode));
+};
