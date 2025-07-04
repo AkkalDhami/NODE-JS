af16c6da3c0fe769a3566b1dcf1399becc17573b
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-04 07:38:51 +0545

    migrations

--- 44_Cokkies_Sessions_in_url_shortener/drizzle/migrations/0000_nervous_bedlam.sql
+++ 44_Cokkies_Sessions_in_url_shortener/drizzle/migrations/0000_nervous_bedlam.sql
@@ -2,7 +2,7 @@ CREATE TABLE `shortLinks` (
 	`id` serial AUTO_INCREMENT NOT NULL,
 	`short_code` varchar(25) NOT NULL,
 	`url` varchar(255) NOT NULL,
-	`created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`created_at` timestamp NOT NULL DEFAULT (now()),
 	CONSTRAINT `shortLinks_id` PRIMARY KEY(`id`),
 	CONSTRAINT `shortLinks_short_code_unique` UNIQUE(`short_code`)
 );
@@ -12,8 +12,8 @@ CREATE TABLE `users` (
 	`name` varchar(255) NOT NULL,
 	`email` varchar(255) NOT NULL,
 	`password` varchar(255) NOT NULL,
-	`created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`created_at` timestamp NOT NULL DEFAULT (now()),
+	`updated_at` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
 	CONSTRAINT `users_id` PRIMARY KEY(`id`),
 	CONSTRAINT `users_email_unique` UNIQUE(`email`)
 );
