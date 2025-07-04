dcfdc819a6c557b8e1d6ee5bb1e59c047799d625
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-04 07:38:58 +0545

    Update schema.js

--- 44_Cokkies_Sessions_in_url_shortener/drizzle/schema.js
+++ 44_Cokkies_Sessions_in_url_shortener/drizzle/schema.js
@@ -1,18 +1,18 @@
-import { sql } from 'drizzle-orm';
-import { mysqlTable, serial, varchar, datetime } from 'drizzle-orm/mysql-core';
+import { mysqlTable, serial, varchar, timestamp } from 'drizzle-orm/mysql-core';
 
 export const shortLinksTable = mysqlTable('shortLinks', {
     id: serial().primaryKey(),
     shortCode: varchar("short_code", { length: 25 }).notNull().unique(),
     url: varchar({ length: 255 }).notNull(),
-    createdAt: datetime('created_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
 });
 
 export const usersTable = mysqlTable('users', {
-    id: serial().primaryKey(),
-    name: varchar({ length: 255 }).notNull(),
-    email: varchar({ length: 255 }).notNull().unique(),
-    password: varchar({ length: 255 }).notNull(),
-    createdAt: datetime('created_at').notNull().default(sql`CURRENT_TIMESTAMP`),
-    updatedAt: datetime('updated_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 255 }).notNull(),
+    email: varchar('email', { length: 255 }).notNull().unique(),
+    password: varchar('password', { length: 255 }).notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().onUpdateNow().notNull(),
 });
+
