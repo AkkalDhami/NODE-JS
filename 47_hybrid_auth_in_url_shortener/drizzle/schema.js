ed3a57e51b49c76fcd7e4df9bf0da48f7f195a2c
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-10 08:51:40 +0545

    Create schema.js

---
+++ 47_hybrid_auth_in_url_shortener/drizzle/schema.js
@@ -0,0 +1,61 @@
+import { relations } from 'drizzle-orm';
+import {
+    mysqlTable,
+    boolean,
+    text,
+    int,
+    serial,
+    varchar,
+    timestamp
+} from 'drizzle-orm/mysql-core';
+
+export const shortLinksTable = mysqlTable('short_links', {
+    id: serial().primaryKey(),
+    shortCode: varchar("short_code", { length: 25 }).notNull().unique(),
+    url: varchar({ length: 255 }).notNull(),
+    totalClicks: int('total_clicks').notNull().default(0),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().onUpdateNow().notNull(),
+    userId: int('user_id').notNull().references(() => usersTable.id),
+});
+
+export const sessionsTable = mysqlTable('sessions', {
+    id: int().autoincrement().primaryKey(),
+    userId: int('user_id').notNull().references(() => usersTable.id, { onDelete: 'cascade' }),
+    valid: boolean().notNull().default(true),
+    uaerAgent: text('user_agent'),
+    ip: varchar({ length: 255 }),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().onUpdateNow().notNull(),
+})
+
+export const usersTable = mysqlTable('users', {
+    id: int('id').autoincrement().primaryKey(),
+    name: varchar('name', { length: 255 }).notNull(),
+    email: varchar('email', { length: 255 }).notNull().unique(),
+    password: varchar('password', { length: 255 }).notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().onUpdateNow().notNull(),
+});
+
+// a user can have many short links
+export const usersRelation = relations(usersTable, ({ many }) => ({
+    shortLink: many(shortLinksTable),
+    session: many(sessionsTable),
+}));
+
+// a short link belongs to a user
+export const shortLinksRelation = relations(shortLinksTable, ({ one }) => ({
+    user: one(usersTable, {
+        fields: [shortLinksTable.userId], // foreign key
+        references: [usersTable.id],
+    })
+}));
+
+// a session belongs to a user
+export const sessionsRelation = relations(sessionsTable, ({ one }) => ({
+    user: one(usersTable, {
+        fields: [sessionsTable.userId], // foreign key
+        references: [usersTable.id],
+    })
+}));
