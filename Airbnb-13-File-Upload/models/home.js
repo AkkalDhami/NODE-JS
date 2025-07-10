a4ece888cbd37c4e46505e8902a15ac7ca4e9cae
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-10 08:49:39 +0545

    add models- file upload

---
+++ Airbnb-13-File-Upload/models/home.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+let homeSchema = new mongoose.Schema({
+    houseName: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    markedPrice: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    houseImage: {
+        type: String
+    },
+    description: {
+        type: String,
+    },
+    rulesPdf: {
+        type: String
+    }
+});
+
+export default mongoose.model('Home', homeSchema);
