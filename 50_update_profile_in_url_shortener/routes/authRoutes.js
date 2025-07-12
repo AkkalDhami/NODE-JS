4905ba542f9757ca7812c67953bed8ac4832d55f
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-12 09:05:08 +0545

    update profile page

---
+++ 50_update_profile_in_url_shortener/routes/authRoutes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { getLogin, getRegister, postLogin, postRegister, getProfile, logout, getVerifyEmail, getResendVerificationLink, getVerifyEmailToken, getEditProfile, postEditProfile } from "../controllers/authController.js";
+
+const router = Router();
+
+router.get('/register', getRegister);
+router.get('/login', getLogin);
+router.get('/profile', getProfile);
+router.get('/edit-profile/:id', getEditProfile);
+
+router.get('/logout', logout);
+router.get('/verify-email', getVerifyEmail);
+router.get('/verify-email-token', getVerifyEmailToken);
+
+
+router.post('/edit-profile/', postEditProfile);
+router.post('/login', postLogin);
+router.post('/register', postRegister);
+router.post('/resend-verification-link', getResendVerificationLink);
+
+export const authRoutes = router;
