1d633ed6336d97d07a594e35e950c070fd2e6657
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-16 08:05:25 +0545

    login with google initial

---
+++ 53_login_with_google_in_url_shortener/controllers/authController.js
@@ -0,0 +1,292 @@
+
+
+import { decodeIdToken, generateCodeVerifier, generateState } from "arctic";
+import { autenticateUser, clearUserSession, clearVerifyEmailToken, createUser, createUserWithOauth, findUserById, findVerificationEmailToken, getAllShortLinksByUserId, getUserByEmail, getUserWithOauthId, hashedPassword, linkUserWithOauth, sendNewVerifyEmailLink, updatePassword, updateUserName, verifyPassword, verifyUserEmailAndUpdate } from "../services/authService.js";
+import { changePasswordSchema, loginSchema, registerSchema, verifyEmailSchema, verifyProfileSchema } from "../validators/authValidators.js";
+import { OAUTH_EXCHANGE_EXPIRY } from "../config/constants.js";
+import { google } from "../lib/oauth/google.js";
+
+
+export const getRegister = (req, res) => {
+    res.render('auth/register', {
+        title: 'Register',
+        errors: req.flash('errors')
+    });
+}
+
+export const getLogin = (req, res) => {
+    res.render('auth/login', {
+        title: 'Login',
+        errors: req.flash('errors')
+    });
+}
+
+export const getProfile = async (req, res) => {
+    if (!req.user) return res.redirect('/auth/login');
+    const user = await findUserById(req.user.id);
+    if (!user) return res.redirect('/auth/login');
+    console.log(user)
+    const allShortLinks = await getAllShortLinksByUserId(user.id);
+    res.render('auth/profile', {
+        title: 'Profile Page',
+        user,
+        links: allShortLinks
+    });
+}
+
+export const getEditProfile = async (req, res) => {
+    const user = await findUserById(req.params.id);
+    if (!user) return res.redirect('/auth/login');
+    res.render('auth/edit-profile', {
+        title: 'Edit Profile Page',
+        user,
+        errors: req.flash('errors')
+    });
+}
+
+export const getChangePasswordPage = (req, res) => {
+    res.render('auth/change-password', {
+        title: 'Change Password',
+        errors: req.flash('errors')
+    });
+}
+
+export const postEditProfile = async (req, res) => {
+
+    const { data, error } = verifyProfileSchema.safeParse(req.body);
+
+    console.log(data)
+
+    if (error) {
+        const errors = error.errors[0].message
+        req.flash('errors', errors);
+        return res.redirect(`/auth/edit-profile/${req.user.id}`);
+    }
+    const { name } = data;
+
+    const updatedUser = await findUserById(req.user.id);
+    if (!updatedUser) return res.redirect('/auth/login');
+    updateUserName(updatedUser.id, name);
+    res.redirect('/auth/profile');
+}
+
+export const logout = async (req, res) => {
+    await clearUserSession(req.user.sessionId);
+    res.clearCookie('access_token');
+    res.clearCookie('refresh_token');
+    res.redirect('/auth/login');
+}
+
+export const getVerifyEmail = (req, res) => {
+    if (!req.user || req.user.isEmailValid) return res.redirect('/');
+    res.render('auth/verify-email', {
+        title: 'Verify Email',
+        user: req.user
+    });
+}
+
+export const getVerifyEmailToken = async (req, res) => {
+    const { data, error } = verifyEmailSchema.safeParse(req.query);
+
+    if (error) return res.redirect('/auth/verify-email');
+
+    const token = await findVerificationEmailToken(data);
+    console.log("token: ", token);
+    if (!token) return res.send('Invalid token');
+
+    await verifyUserEmailAndUpdate(token.email);
+
+    clearVerifyEmailToken(token.email).catch(console.error);
+
+    return res.redirect('/auth/profile');
+}
+
+export const getResendVerificationLink = async (req, res) => {
+    if (!req.user || req.user.isEmailValid) return res.redirect('/');
+
+    await sendNewVerifyEmailLink({ userId: req.user.id, email: req.user.email }).catch(console.error);
+    res.redirect('/auth/verify-email');
+}
+
+export const getGoogleLoginPage = async (req, res) => {
+    if (req.user) return res.redirect('/');
+    const state = generateState();
+    const codeVerifier = generateCodeVerifier();
+    const url = google.createAuthorizationURL(state, codeVerifier, [
+        "openid",
+        "profile",
+        "email"
+    ]);
+
+    const cookieConfig = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        maxAge: OAUTH_EXCHANGE_EXPIRY
+    };
+
+    res.cookie('google_oauth_state', state, cookieConfig);
+    res.cookie('google_oauth_code_verifier', codeVerifier, cookieConfig);
+
+    res.redirect(url.toString());
+}
+
+export const getGoogleLoginCallbackPage = async (req, res) => {
+    if (req.user) return res.redirect('/');
+
+    const { code, state } = req.query;
+    const { google_oauth_state: storedState, google_oauth_code_verifier: codeVerifier } = req.cookies;
+
+    if (
+        !code ||
+        !state ||
+        !storedState ||
+        !codeVerifier ||
+        state !== storedState
+    ) {
+        req.flash(
+            "errors",
+            "Couldn't login with Google because of invalid login attempt. Please try again!"
+        );
+        return res.redirect("/login");
+    }
+
+    let tokens;
+    try {
+        tokens = await google.validateAuthorizationCode(code, codeVerifier);
+    } catch {
+        req.flash(
+            "errors",
+            "Couldn't login with Google because of invalid login attempt. Please try again!"
+        );
+        return res.redirect("/login");
+    }
+    console.log("token google: ", tokens);
+
+    const claims = decodeIdToken(tokens.idToken());
+    const { sub: googleUserId, name, email } = claims;
+
+    // if user is already linked then we will get the user
+    let user = await getUserWithOauthId({
+        provider: "google",
+        email,
+    });
+
+    // if user exists but user is not linked with oauth
+    if (user && !user.providerAccountId) {
+        await linkUserWithOauth({
+            userId: user.id,
+            provider: "google",
+            providerAccountId: googleUserId,
+        });
+    }
+
+    // if user doesn't exist
+    if (!user) {
+        user = await createUserWithOauth({
+            name,
+            email,
+            provider: "google",
+            providerAccountId: googleUserId,
+        });
+    }
+    await autenticateUser({ req, res, user });
+
+    res.redirect("/");
+}
+
+export const postLogin = async (req, res) => {
+    if (req.user) return res.redirect('/');
+    const { data, error } = loginSchema.safeParse(req.body);
+    if (error) {
+        const errors = error.errors[0].message
+        req.flash('errors', errors);
+        return res.redirect('/auth/login');
+    }
+    const { email, password } = data;
+
+    try {
+        if (!email || !password) {
+            req.flash('errors', 'Email or password is missing');
+            return res.redirect('/auth/login');
+        }
+        const user = await getUserByEmail(email);
+        if (!user) {
+            req.flash('errors', 'Invalid Credentials');
+            return res.redirect('/auth/login');
+        }
+
+        if (!user.password) {
+            req.flash('errors', 'You have created your account with social login. Please login with social account.');
+            return res.redirect('/auth/login');
+        }
+
+        const isPasswordValid = await verifyPassword(password, user.password);
+        if (!isPasswordValid) {
+            req.flash('errors', 'Invalid Credentials');
+            return res.redirect('/auth/login');
+        }
+
+        await autenticateUser({ req, res, user });
+
+    } catch (error) {
+        console.error('Error during login:', error);
+        return res.redirect('/auth/login');
+    }
+    res.redirect('/');
+}
+
+export const postRegister = async (req, res) => {
+    if (req.user) return res.redirect('/');
+    const { data, error } = registerSchema.safeParse(req.body);
+    if (error) {
+        const errors = error.errors[0].message
+        req.flash('errors', errors);
+        return res.redirect('/auth/register');
+    }
+    const { name, email, password } = data;
+    try {
+        const userExists = await getUserByEmail(email);
+        if (userExists) {
+            req.flash('errors', 'User already exists');
+            return res.redirect('/auth/register');
+        }
+
+        const hashedPasswd = await hashedPassword(password);
+        const [user] = await createUser({ name, email, password: hashedPasswd });
+        await autenticateUser({ req, res, user });
+        await sendNewVerifyEmailLink({ userId: user.id, email }).catch(console.error);
+        return res.redirect('/');
+    } catch (error) {
+        console.error('Error during registration:', error);
+        return res.redirect('/auth/register');
+    }
+}
+
+
+export const postChangePassword = async (req, res) => {
+    const { data, error } = changePasswordSchema.safeParse(req.body);
+    if (error) {
+        const errors = error.errors[0].message
+        req.flash('errors', errors);
+        return res.redirect('/auth/change-password');
+    }
+
+    const user = await findUserById(req.user.id);
+    if (!user) return res.redirect('/auth/login');
+    // get current password, new password
+    const { currentPassword, newPassword } = data;
+
+    // check if current password is correct
+    const isPasswordValid = await verifyPassword(currentPassword, user.password);
+    if (!isPasswordValid) {
+        req.flash('errors', 'Current password is incorrect');
+        return res.redirect('/auth/change-password');
+    }
+
+    // update password
+    const hashedPasswd = await hashedPassword(newPassword);
+    await updatePassword(user.id, hashedPasswd);
+
+    res.redirect('/auth/profile');
+}
