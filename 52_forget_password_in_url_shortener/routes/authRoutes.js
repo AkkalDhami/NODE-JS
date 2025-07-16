56c854b8bb2df57e3dbe4e32f41befd94aea52b7
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-16 08:04:27 +0545

    add forget password functionality

---
+++ 52_forget_password_in_url_shortener/routes/authRoutes.js
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { getLogin, getRegister, postLogin, postRegister, getProfile, logout, getVerifyEmail, getResendVerificationLink, getVerifyEmailToken, getEditProfile, postEditProfile, getChangePasswordPage, postChangePassword, getGoogleLoginPage, getGoogleLoginCallbackPage, getResetPasswordPage, postForgetPassword, getResetPasswordTokenPage, postResetPassword } from "../controllers/authController.js";
+
+const router = Router();
+
+router.get('/register', getRegister);
+router.get('/login', getLogin);
+router.get('/profile', getProfile);
+router.get('/edit-profile/:id', getEditProfile);
+
+router.get('/change-password', getChangePasswordPage);
+
+router.get('/logout', logout);
+router.get('/verify-email', getVerifyEmail);
+router.get('/verify-email-token', getVerifyEmailToken);
+
+router.route('/reset-password').get(getResetPasswordPage).post(postForgetPassword);
+router.route('/reset-password/:token')
+    .get(getResetPasswordTokenPage)
+    .post(postResetPassword);
+
+router.route('/google').get(getGoogleLoginPage);
+router.route('/google/callback').get(getGoogleLoginCallbackPage);
+
+router.post('/edit-profile/', postEditProfile);
+router.post('/change-password', postChangePassword);
+router.post('/login', postLogin);
+router.post('/register', postRegister);
+router.post('/resend-verification-link', getResendVerificationLink);
+
+export const authRoutes = router;
